36c05fa8c8e386af5340b4015ed5262d88648a38
Wint567 <134635875+Wint567@users.noreply.github.com>
2023-10-23 17:11:39 +0300

    task22

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 const emojis = ['😄', '😃', '😁', '😆', '😅'];
-        const voteCounts = Array(emojis.length).fill(0);
+const voteCounts = Array(emojis.length).fill(0);
 
-        const votingResultsDiv = document.getElementById('votingResults');
+const votingResultsDiv = document.getElementById('votingResults');
 
-        function displayResults() {
-            votingResultsDiv.innerHTML = '';
-            emojis.forEach((emoji, index) => {
-                const emojiDiv = document.createElement('div');
-                emojiDiv.classList.add('emoji');
-                emojiDiv.textContent = emoji;
-                emojiDiv.addEventListener('click', () => {
-                    voteCounts[index]++;
-                    displayResults();
-                });
-                votingResultsDiv.appendChild(emojiDiv);
-                votingResultsDiv.insertAdjacentHTML('beforeend', `${voteCounts[index]}`);
-            });
+function displayResults() {
+    votingResultsDiv.innerHTML = '';
+    emojis.forEach((emoji, index) => {
+        const emojiDiv = document.createElement('div');
+        emojiDiv.classList.add('emoji');
+        emojiDiv.textContent = emoji;
+        votingResultsDiv.appendChild(emojiDiv);
+        votingResultsDiv.insertAdjacentHTML('beforeend', `${voteCounts[index]}`);
+    });
+}
+
+votingResultsDiv.addEventListener('click', function (event) {
+    const target = event.target;
+    if (target.classList.contains('emoji')) {
+        const emojiIndex = emojis.indexOf(target.textContent);
+        if (emojiIndex !== -1) {
+            voteCounts[emojiIndex]++;
+            displayResults();
         }
+    }
+});
 
-        displayResults();
+displayResults();
